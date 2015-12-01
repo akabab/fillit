f82baf7e22df9dce56d442b6ac524c61b94afbe0
akabab <ycribier@student.42.fr>
2015-12-01 16:59:28 +0100

    open and recover fd

--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
+#include "libft.h"
+#include "fillit.h"
+#include <fcntl.h>
+
 int		main(int ac, char *av[])
 {
+	int		fd;
 	
+	if (ac != 2)
+	{
+		ft_printf("Usage..\n");
+		return (-1);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf("Open failed\n");
+		return (-1);
+	}
 	return (0);
 }
