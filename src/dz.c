d4b759ebc46347014ba4e6af44b659f24cd60f45
akabab <ycribier@student.42.fr>
2015-12-08 20:46:18 +0100

    count_adj: removed useless parameter

--- src/dz.c
+++ src/dz.c
@@ -5,7 +5,7 @@ int			get_bit_from_int(uint16_t value, int n)
 	return ((value >> (0xF - n)) & 0x1); // vs (value & 0x8000 >> n)
 }
 
-int			count_adj(t_map *map, int x, int y, int adj)
+int			count_adj(t_map *map, int x, int y)
 {
 	int		bit;
 	int		adj_sum;
@@ -14,13 +14,13 @@ int			count_adj(t_map *map, int x, int y, int adj)
 		return (0);
 	bit = get_bit_from_int(map->mdz[y], x);
 	if (bit)
-		return (adj);
+		return (0);
 	map->mdz[y] |= 0x8000 >> x;
 	adj_sum = 1;
-	adj_sum += count_adj(map, x - 1, y, adj);
-	adj_sum += count_adj(map, x, y - 1, adj);
-	adj_sum += count_adj(map, x + 1, y, adj);
-	adj_sum += count_adj(map, x, y + 1, adj);
+	adj_sum += count_adj(map, x - 1, y);
+	adj_sum += count_adj(map, x, y - 1);
+	adj_sum += count_adj(map, x + 1, y);
+	adj_sum += count_adj(map, x, y + 1);
 	return (adj_sum);
 }
 
@@ -39,10 +39,9 @@ t_bool		is_enough_space(t_map *map)
 		x = 0;
 		while (x < map->size)
 		{
-			adj = count_adj(map, x, y, 0);
+			adj = count_adj(map, x, y);
 			if (adj < 4)
 				n_dz += adj;
-			// printf("[%d,%d] adj=%d dz=%d\n", y, x, adj, n_dz);
 			if (map->total_space - n_dz < map->space_required)
 				return (FALSE);
 			x++;
