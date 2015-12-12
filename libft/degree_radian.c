ad3f93a534bbfa21c6383621faa0071a2aaa6e80
akabab <ycribier@student.42.fr>
2015-12-12 12:53:29 +0100

    back to fmod syscall, cannot use % (modulo) on floats

--- libft/degree_radian.c
+++ libft/degree_radian.c
@@ -14,7 +14,7 @@
 
 double	fdeg(double value)
 {
-	value = value % 360;
+	value = fmod(value, 360);
 	if (value < 0)
 		value += 360;
 	return (value);
@@ -22,7 +22,7 @@ double	fdeg(double value)
 
 double	frad(double value)
 {
-	value = value % (2 * M_PI);
+	value = fmod(value, 2 * M_PI);
 	if (value < 0)
 		value += 2 * M_PI;
 	return (value);
